25bf112f223f9c2ce71e1aaa8e3c347dbdf0c399
imparasparabiya <137284876+imparasparabiya@users.noreply.github.com>
2023-06-21 16:56:30 +0530

    Add files via upload

---
+++ CH_1BOI.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+
+{
+   clrscr();
+   printf("My Name :- PARAS PARABIYA\n");
+   printf("Education :- B.com\n");
+   printf("Job :- Accounter in Construction Compny\n");
+   getch();
+
+}
---
+++ CH_1HELL.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+
+{
+  clrscr();
+  printf("Hello...World");
+  getch();
+
+}
